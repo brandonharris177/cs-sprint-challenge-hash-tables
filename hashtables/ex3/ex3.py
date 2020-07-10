9990b8db72a4cdaca0f834760650bec628502f3f
Brandon-Harris <brandonharris177@gmail.com>
2020-07-10 10:26:25 -0600

    example 3 tests passing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,33 @@
+import operator
+
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
     # Your code here
+
+    count = 0
+    for each_list in arrays:
+        count += 1
+
+    intersections = {}
+
+    for single_list in arrays:
+        for number in single_list:
+            if number not in intersections:
+                intersections[number] = 1
+            else: 
+                intersections[number] += 1
+
+    flipped = {} 
+  
+    for key, value in intersections.items(): 
+        if value not in flipped: 
+            flipped[value] = [key] 
+        else: 
+            flipped[value].append(key) 
+
+    result = flipped[count]
 
     return result
 
@@ -13,5 +38,11 @@ if __name__ == "__main__":
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+
+    # arrays.append([1,2,3]),
+    # arrays.append([1,3,5]),
+    # arrays.append([1,6,7])
+
 
     print(intersection(arrays))
