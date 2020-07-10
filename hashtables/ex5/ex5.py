34419d8654016b00dedee5cc644f3f2b606fbedd
Brandon-Harris <brandonharris177@gmail.com>
2020-07-10 11:29:26 -0600

    example 5 all tests passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,23 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    file_paths = {}
+
+    for total_file in files:
+        file_key = total_file.split("/")
+        file_key = file_key[len(file_key)-1]
+        if file_key not in file_paths:
+            file_paths[file_key] = []
+        file_paths[file_key].append(total_file)
+
+    # print(file_paths)
+
+    result = []
+
+    for query in queries:
+        if query in file_paths:
+            for path in file_paths[query]:
+                result.append(path)
 
     return result
 
