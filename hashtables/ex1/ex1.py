ddf2f5f5798aa1de9adcc049119f86a9801e2079
Brandon-Harris <brandonharris177@gmail.com>
2020-07-10 11:59:26 -0600

    some work done on example 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
-    return None
+    hash_table = {}
+
+    count = 0
+
+    for weight in weights:
+        hash_table[weight] = count
+        count += 1
+
+    return(hash_table)
+
+    # return result
+
+weights_3 = [4, 6, 10, 15, 16]
+print(get_indices_of_item_weights(weights_3, 5, 21))
